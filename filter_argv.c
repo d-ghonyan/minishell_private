81b4a45a6e79e70e678bd8fe6d3b6d1520508970
Davit Ghonyan <dghonyan@c2r16s5.42yerevan.am>
2022-09-01 19:08:38 +0400

    quotes in argv

--- filter_argv.c
+++ filter_argv.c
@@ -6,7 +6,7 @@
 /*   By: dghonyan <dghonyan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/10 20:19:51 by dghonyan          #+#    #+#             */
-/*   Updated: 2022/09/01 18:58:10 by dghonyan         ###   ########.fr       */
+/*   Updated: 2022/09/01 19:08:37 by dghonyan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ char	**filter_argv(char **argv, t_cmd *cmd, int k)
 	int		i;
 	int		j;
 	char	**new_argv;
-	
+
 	new_argv = malloc(sizeof (*new_argv) * (ptr_arr_len(argv)));
 	perror_exit(cmd, "malloc at filter_argv", !new_argv);
 	j = 0;
